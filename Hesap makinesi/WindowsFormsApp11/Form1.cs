cfd14d7e9e02903367c5865d4cbc7f6dbd4f11f2
HayrullahT <tonbulhayrullah123@gmail.com>
2023-03-14 10:13:36 -0400

    Hesap makinesi uygulaması
    C# ile yapılmıştır.

---
+++ Hesap makinesi/WindowsFormsApp11/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        İslemler aa = new İslemler();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+label2.Text = aa.Toplama(double.Parse(textBox1.Text), double.Parse(textBox2.Text)).ToString();
+       
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Muhtemelen sayı girmediniz!");
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+label2.Text = aa.Cıkarma(double.Parse(textBox1.Text), double.Parse(textBox2.Text)).ToString();
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Muhtemelen sayı girmediniz!");
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+label2.Text = aa.Carpma(double.Parse(textBox1.Text), double.Parse(textBox2.Text)).ToString();
+            
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Muhtemelen sayı girmediniz!");
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                label2.Text = aa.Bolme(int.Parse(textBox1.Text), int.Parse(textBox2.Text)).ToString();
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Muhtemelen sayı girmediniz!");
+               
+            }    
+        }
+    }
+}
---
+++ Hesap makinesi/WindowsFormsApp11/İslemler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp11
+{
+    class İslemler
+    {
+
+        public double Toplama(double sayi1,double sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public double Cıkarma(double sayi1, double sayi2)
+        {
+            return sayi1 - sayi2;
+        }
+        public double Carpma(double sayi1, double sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+        public double Bolme(double sayi1, double sayi2)
+        {
+            return sayi1/sayi2;
+        }
+    }
+}
